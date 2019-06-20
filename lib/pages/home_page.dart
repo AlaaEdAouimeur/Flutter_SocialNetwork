c9a8e42f53178c498fa84f2b394067e00c4f55e6
Vaibhav Rai <vrai@livelike.com>
2019-06-21 01:43:30 +0530

    WIP: Saving user data to local database after it is stored to firebase Adding icons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import '../database/databaseReferences.dart' as databaseReference;
 import '../widgets/tab_view_controller.dart' as tabController;
 import 'package:redux_example/appColors.dart' as AppColor;
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -15,43 +15,10 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: appBar(),
       body: tabController.TabViewController().showBody(_selectedIndex),
       bottomNavigationBar: bottomNavigator(),
       backgroundColor: AppColor.offWhite,
     );
-  }
-
-  void deleteBox(String user) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Delete!"),
-            content:
-                Text("Do you really want to delete this entry from database?"),
-            actions: <Widget>[
-              FlatButton(
-                child: Text("Delete"),
-                onPressed: () {
-                  databaseReference.DatabaseReferences()
-                      .parentReference
-                      .child(user)
-                      .remove()
-                      .then((_) => {
-                            print("user deleted"),
-                            Navigator.of(context).pop(),
-                          });
-                },
-              ),
-              FlatButton(
-                  child: Text("Back"),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  }),
-            ],
-          );
-        });
   }
 
   Widget appBar() {
@@ -83,17 +50,31 @@ class _HomePageState extends State<HomePage> {
     return new PreferredSize(
       preferredSize: Size.fromHeight(40.0),
       child: BottomNavigationBar(
-        fixedColor: AppColor.offWhite,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.black,
+        unselectedItemColor: Colors.white30,
         iconSize: 15,
         items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
-          BottomNavigationBarItem(icon: Icon(Icons.book), title: Text("Blog")),
           BottomNavigationBarItem(
-              icon: Icon(Icons.note_add), title: Text("Write")),
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+          ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.search), title: Text("Search")),
+            icon: Icon(Icons.book),
+            title: Text("Blog"),
+          ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.person), title: Text("User")),
+            icon: Icon(Icons.note_add),
+            title: Text("Write"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text("Search"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text("User"),
+          ),
         ],
         currentIndex: _selectedIndex,
         type: BottomNavigationBarType.fixed,
--- lib/widgets/homeTab.dart
+++ lib/widgets/homeTab.dart
@@ -2,31 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_database/firebase_database.dart';
 import '../database/databaseReferences.dart' as databaseReference;
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 
 class HomeTab extends StatefulWidget {
   HomeTab({Key key}) : super(key: key);
+
   @override
   _HomeTabState createState() => _HomeTabState();
 }
 
 class _HomeTabState extends State<HomeTab> {
-  
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Flexible(
-          child: FirebaseAnimatedList(
-              query: databaseReference.DatabaseReferences().postDatabaseReference,
-              itemBuilder: (BuildContext context, DataSnapshot snapshot,
-                  Animation<double> animation, int index) {
-                return new Container(
-                  margin: EdgeInsets.only(
-                      left: 40.0, right: 40.0, top: 10.0, bottom: 10.0),
-                  child: shortPostBody(snapshot),
-                );
-              }),
-        ),
-      ],
+    return Container(
+      color: Colors.black,
+      child: Column(
+        children: <Widget>[
+          Flexible(
+            child: FirebaseAnimatedList(
+                query: databaseReference.DatabaseReferences()
+                    .postDatabaseReference,
+                itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                    Animation<double> animation, int index) {
+                  return new Container(
+//                    color: Colors.red,
+                    margin: EdgeInsets.only(
+                        left: 25.0, right: 25.0, top: 20.0, bottom: 10.0),
+                    child: shortPostBody(snapshot),
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 
@@ -37,20 +43,39 @@ class _HomeTabState extends State<HomeTab> {
         Text(
           snapshot.value['writeup'],
           style: TextStyle(
-            color: Colors.grey,
-            letterSpacing: 0.5,
-            height: 1.2,
-          ),
-          textAlign: TextAlign.justify,
+              color: Colors.white,
+              letterSpacing: 0.1,
+              height: 1.1,
+              fontSize: 18),
+          textAlign: TextAlign.left,
         ),
-        Text(
-          snapshot.value['name'],
-          style: TextStyle(
-            color: Colors.green,
-            letterSpacing: 1.0,
-            fontSize: 14.0,
-            height: 2.0,
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+//              color: Colors.orange,
+              child: Text(
+                snapshot.value['name'],
+                style: TextStyle(
+                  color: Colors.green,
+                  letterSpacing: 1.0,
+                  fontSize: 14.0,
+                  height: 2.0,
+                ),
+              ),
+            ),
+            Container(
+//              color: Colors.red,
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    EvaIcons.email,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
         Container(
           margin: EdgeInsets.only(top: 10.0, bottom: 20.0),
