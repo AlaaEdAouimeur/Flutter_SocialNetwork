6d31c394ad4a983558ca03596704dceea10149b9
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2019-09-16 17:57:50 +0530

    Modified to handle data input by user

--- lib/HelperClasses/DatabaseHelperClass.dart
+++ lib/HelperClasses/DatabaseHelperClass.dart
@@ -1,36 +1,49 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:redux_example/HelperClasses/FirstLoginHelper.dart';
 import '../database/databaseReferences.dart' as databaseReferences;
 import 'package:firebase_auth/firebase_auth.dart';
 
 class DatabaseHelperClass {
-  Future<DocumentSnapshot> saveUserDataToDatabase(FirebaseUser user) {
-    var value = {
-      "name": user.displayName.toLowerCase(),
-      "email": user.email.toLowerCase(),
-      "isEmailVerified": user.isEmailVerified.toString(),
-      "profilePictureUrl": user.photoUrl.toString(),
-      "uid": user.uid,
-      "createdAt": DateTime.now(),
-      "followers": 0,
-      "followings": 0,
-      "posts": 0,
-      "blogs": 0,
-      "location": null,
-      "username": null,
-      "birthday": null,
-      "location": null,
-      "bio": null,
-    };
+  Future<DocumentSnapshot> saveUserDataToDatabase(FirebaseUser user, BuildContext context) {
     return checkIfUserAlreadyExist(user.email).then((val) async {
-      if (val == null) {
-        print("INITIALIZED USER DETAILS");
-        await databaseReferences.DatabaseReferences()
-            .users
-            .document()
-            .setData(value)
-            .then(
-              (_) => print("User data stored to firestore"),
-            );
+      if (val == null || val['username'] == null) {
+        bool shouldProceed = true;
+        Map<String, dynamic> userUpdate =
+            await FirstLoginHelper.showPopup(context);
+        print('USER DETAILS: ${userUpdate.toString()}');
+        if (userUpdate != null) {
+          userUpdate.forEach((key, value) {
+            if (value == null) shouldProceed = false;
+          });
+        } else
+          shouldProceed = false;
+        print('SHOULD PROCEED: $shouldProceed');
+        if (shouldProceed) {
+          var value = {
+            "name": user.displayName.toLowerCase(),
+            "email": user.email.toLowerCase(),
+            "isEmailVerified": user.isEmailVerified.toString(),
+            "profilePictureUrl": user.photoUrl.toString(),
+            "uid": user.uid,
+            "createdAt": DateTime.now(),
+            "followers": 0,
+            "followings": 0,
+            "posts": 0,
+            "username": userUpdate['username'],
+            "birthday": userUpdate['birthday'],
+            "location": userUpdate['location'],
+            "bio": userUpdate['bio'],
+          };
+          print("INITIALIZED USER DETAILS");
+          await databaseReferences.DatabaseReferences()
+              .users
+              .document()
+              .setData(value)
+              .then(
+                (_) => print("User data stored to firestore"),
+              );
+        }
       }
       return val;
     });
