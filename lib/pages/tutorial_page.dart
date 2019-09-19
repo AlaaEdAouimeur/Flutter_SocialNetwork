5d24909e7e0b236e4d4a24041160f4d0ea7841ba
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2019-09-19 16:15:05 +0530

    Removed an unused package

--- lib/pages/tutorial_page.dart
+++ lib/pages/tutorial_page.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../animations/slideAnimation.dart';
 import '../HelperClasses/TutorialPageHelperClass.dart';
