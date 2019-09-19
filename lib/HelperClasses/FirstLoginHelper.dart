2ae3d92962bf9f19be6213257ff87b926aec5579
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2019-09-19 16:39:26 +0530

    Reduced the padding to 50 from each side

--- lib/HelperClasses/FirstLoginHelper.dart
+++ lib/HelperClasses/FirstLoginHelper.dart
@@ -10,8 +10,8 @@ class FirstLoginHelper {
   static Future<Map<String, dynamic>> showPopup(BuildContext context) {
     return Navigator.of(context).push(
       PopupModal(
-        top: 100,
-        bottom: 100,
+        top: 50,
+        bottom: 50,
         left: 50,
         right: 50,
         background: Color.fromRGBO(0, 0, 0, 0.3),
